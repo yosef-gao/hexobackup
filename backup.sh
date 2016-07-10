a0f731d8dc9a12a056e7dba7b537698456d3095a
yosef gao <1033131660@qq.com>
2016-07-10 13:44:45 +0800

    backup

--- backup.sh
+++ backup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+hexo g > /dev/null
+hexo d >> log.txt 2>&1 
+
 git add -A .
-git commit -m 'backup'
-git push >> log.txt 2>&1 &
+git commit -m 'backup' >> /dev/null
+git push >> log.txt 2>&1
